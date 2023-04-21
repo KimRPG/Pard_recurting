db4478af00b2ca0c2f98d63701e39d122a273989
dasdfadssda <quddkflty@gmail.com>
2023-04-21 17:47:35 +0900

    solve  Hosting error

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import HomePage from './pages/HomePage';
 import InquiryPage from './pages/InquiryPage';
 import ProjectPage from './pages/ProjectPage';
 import RecruitingPage from './pages/RecruitingPage';
-import ScrollToTop from './components/ScrollToTop';
 
 function App() {
-    return <Router>
-        <ScrollToTop/>
-        <Routes>
+    return <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/About" element={<AboutPage />} />
             <Route path="/Inquiry" element={<InquiryPage />} />
             <Route path="/Recruting" element={<RecruitingPage />} />
             <Route path='/Project' element={<ProjectPage />} />
         </Routes>
-    </Router>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+    <ScrollToTop/>
+        <App />
+    </BrowserRouter>
 );
